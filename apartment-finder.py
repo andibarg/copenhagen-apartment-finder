f2faeb46b83b3325583cd7983c082cbcc58cd849
Romain Briche <romain.briche@gmail.com>
2016-06-20 21:50:49 +0200

    Refactor User class into a Watcher class

--- apartment-finder.py
+++ apartment-finder.py
@@ -22,7 +22,7 @@ DBA_URL = 'EDIT ME' # For example, http://www.dba.dk/boliger/andelsbolig/andelsl
 BOLIGA_URL = 'EDIT ME' # For example, http://www.boliga.dk/soeg/resultater/a194d60c-e272-4c09-a7fd-899763577c58?sort=liggetid-a')
 
 
-class User(object):
+class Watcher(object):
     def __init__(self, email_recipients, dba_url, boliga_url=None, bcc_recipients=False):
         self.email_recipients = email_recipients
         self.bcc_recipients = bcc_recipients
@@ -49,10 +49,10 @@ def send_email(headline, url, recipients, bcc):
     smtp_obj.sendmail(FROM, TO, message)
     smtp_obj.close()
 
-def crawl_dba(user):
+def crawl_dba(watcher):
     session = requests.Session()
     session.encoding = 'utf-8'
-    r = session.get(user.dba_url)
+    r = session.get(watcher.dba_url)
     soup = BeautifulSoup(r.text, convertEntities=BeautifulSoup.HTML_ENTITIES)
     listings = soup.findAll('tr', 'dbaListing')
     for listing in listings:
@@ -61,40 +61,42 @@ def crawl_dba(user):
         headline = a.contents[0].encode('utf-8')
         if headline is None:
             headline = link
-        process_property(headline, link, user)
+        process_property(headline, link, watcher)
 
-def crawl_boliga(user):
+def crawl_boliga(watcher):
     session = requests.Session()
     session.encoding = 'utf-8'
-    r = session.get(user.boliga_url)
+    r = session.get(watcher.boliga_url)
     soup = BeautifulSoup(r.text.encode('utf-8'), convertEntities=BeautifulSoup.HTML_ENTITIES)
     listings = soup.findAll('tr', 'pRow')
     for listing in listings:
         link_element = listing.findAll('a')[1]
         headline = link_element['title'].encode('utf-8')
         url = ('http://www.boliga.dk' + link_element['href']).encode('utf-8')
-        process_property(headline, url, user)
+        process_property(headline, url, watcher)
 
-def process_property(headline, url, user):
-    if url not in user.seen_urls:
-        if not user.first_run:
-            send_email(headline, url, user.email_recipients, user.bcc_recipients)
-        user.seen_urls.add(url)
+def process_property(headline, url, watcher):
+    if url not in watcher.seen_urls:
+        if not watcher.first_run:
+            send_email(headline, url, watcher.email_recipients, watcher.bcc_recipients)
+        watcher.seen_urls.add(url)
         print 'Property found: %s - %s' % (headline, url)
 
 def main():
-    user = User(email_recipients=TARGET_EMAILS,
-                dba_url=DBA_URL,
-                boliga_url=BOLIGA_URL,
-                bcc_recipients=BCC_TARGETS)
+    watcher = Watcher(
+        email_recipients=TARGET_EMAILS,
+        dba_url=DBA_URL,
+        boliga_url=BOLIGA_URL,
+        bcc_recipients=BCC_TARGETS
+    )
 
     while True:
         try:
-            crawl_dba(user)
-            crawl_boliga(user)
-            if user.first_run:
-                print 'First run passed and fetched %d properties' % len(user.seen_urls)
-                user.first_run = False
+            crawl_dba(watcher)
+            crawl_boliga(watcher)
+            if watcher.first_run:
+                print 'First run passed and fetched %d properties' % len(watcher.seen_urls)
+                watcher.first_run = False
             time.sleep(REFRESH_INTERVAL_SECONDS)
         except Exception as e:
             print e
